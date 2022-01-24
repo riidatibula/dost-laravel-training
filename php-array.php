42d14fa80a487fc2d48c136a78ddd870281d25f9
Romualdo D. Atibula <riidatibula@gmail.com>
2022-01-24 16:43:56 +0800

    added homepage and fixed spacing

--- php-array.php
+++ php-array.php
@@ -57,37 +57,34 @@ $sports_cars = [
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-    <title>Sports Cars</title>
-  </head>
-  </head>
-  <body>
-      
-    <table class="table-bordered table-striped table-hover table-sm">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Capacity</th>
-                <th>Description</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($sports_cars as $sports_car) { ?>
-            <tr>
-                <td><?php echo $sports_car->getName(); ?></td>
-                <td><?php echo $sports_car->getCapacity(); ?></td>
-                <td><?php echo $sports_car->getDescription(); ?></td>
-            </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-
-  </body>
+        <title>Sports Cars</title>
+    </head>
+    <body>
+        <table class="table-bordered table-striped table-hover table-sm">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Capacity</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($sports_cars as $sports_car) { ?>
+                <tr>
+                    <td><?php echo $sports_car->getName(); ?></td>
+                    <td><?php echo $sports_car->getCapacity(); ?></td>
+                    <td><?php echo $sports_car->getDescription(); ?></td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </body>
 </html>
