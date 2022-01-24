97e295b496a892088f0ae8dccea0a7b1db69e15d
Romualdo D. Atibula <riidatibula@gmail.com>
2022-01-24 16:56:18 +0800

    fixed tabulation

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>DOST Laravel Training</title>
-</head>
-<body>
-    <h1>DOST Laravel Training</h1>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>DOST Laravel Training</title>
+    </head>
+    <body>
+        <h1>DOST Laravel Training</h1>
 
-    <h3>Day 1</h3>
-    <ul>
-        <li><a href="asyncjs.html">Async JS example</a></li>
-        <li><a href="php-array.php">PHP array example</a></li>
-    </ul>
-</body>
+        <h3>Day 1</h3>
+        <ul>
+            <li><a href="asyncjs.html">Async JS example</a></li>
+            <li><a href="php-array.php">PHP array example</a></li>
+        </ul>
+    </body>
 </html>
