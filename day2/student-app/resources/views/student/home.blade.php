da9fbc1aebd34282bda99773e3a5d40230166a43
Romualdo D. Atibula <riidatibula@gmail.com>
2022-01-25 16:43:18 +0800

    added homepage initial implementation

--- day2/student-app/resources/views/student/home.blade.php
+++ day2/student-app/resources/views/student/home.blade.php
@@ -1,1 +1,41 @@
-<h1>Home Page</h1>
+@extends('student.base')
+
+@section('content')
+
+    <div class="container-fluid">
+        <div class="home">
+            <div class="row mb-3">
+                <div class="col">
+                    <h1>Hello, Ada. What do you want to do today?</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Profile</h5>
+                            <a href="{{ route('profile') }}" class="btn btn-sm btn-primary">View</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Enrolled Subjects</h5>
+                            <a href="{{ route('enrolled_subjects') }}" class="btn btn-sm btn-primary">View</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Grades</h5>
+                            <a href="{{ route('grades') }}" class="btn btn-sm btn-primary">View</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection()
