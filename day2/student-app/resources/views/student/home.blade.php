59d0f16e7493e88f85ecdba2b6ddf74b108fabe1
Romualdo D. Atibula <riidatibula@gmail.com>
2022-01-26 11:26:42 +0800

    updated homepage

--- day2/student-app/resources/views/student/home.blade.php
+++ day2/student-app/resources/views/student/home.blade.php
@@ -11,25 +11,28 @@
             </div>
             <div class="row">
                 <div class="col">
-                    <div class="card">
+                    <div class="card border-info">
                         <div class="card-body">
                             <h5 class="card-title">Profile</h5>
+                            <p class="card-text">View your latest information such as your course and year level</p>
                             <a href="{{ route('profile') }}" class="btn btn-sm btn-primary">View</a>
                         </div>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card">
+                    <div class="card border-info">
                         <div class="card-body">
                             <h5 class="card-title">Enrolled Subjects</h5>
+                            <p class="card-text">View your enrolled subjects and schedule for the current semester</p>
                             <a href="{{ route('enrolled_subjects') }}" class="btn btn-sm btn-primary">View</a>
                         </div>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card">
+                    <div class="card border-info">
                         <div class="card-body">
                             <h5 class="card-title">Grades</h5>
+                            <p class="card-text">View your midterm and final grade for the current semester</p>
                             <a href="{{ route('grades') }}" class="btn btn-sm btn-primary">View</a>
                         </div>
                     </div>
